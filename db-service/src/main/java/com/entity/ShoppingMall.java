40261ad2b53d2520647357db0a41b7d49d56f708
DenysFlnk <filonenko.denys94@gmail.com>
2024-12-16 16:43:22 +0200

    add shopping_mall table and relations with hobby

---
+++ db-service/src/main/java/com/entity/ShoppingMall.java
@@ -0,0 +1,68 @@
+package com.entity;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import java.util.List;
+import org.locationtech.jts.geom.Point;
+
+@Entity(name = "shopping_mall")
+public class ShoppingMall extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private Point location;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "shopping_mall_hobby",
+        joinColumns = @JoinColumn(name = "shopping_mall_id"),
+        inverseJoinColumns = @JoinColumn(name = "hobby_id")
+    )
+    private List<Hobby> hobbies;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public List<Hobby> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<Hobby> hobbies) {
+        this.hobbies = hobbies;
+    }
+}
